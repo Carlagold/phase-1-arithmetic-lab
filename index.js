842be14d8973d430a9d11ea246a14af4ecc0fb4b
Carla Sachetti <csachett@student.scf.edu>
2022-07-14 00:44:36 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-
 // Write your code here
+let multiply = 62;
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2;
+
+let random = Math.floor(Math.random()) + 1;
+
+let mod = 4;
+let num3 = 28;
+let num4 = 8;
+mod = num3 % num4;
+
+let max = Math.max([20]);
+
+
